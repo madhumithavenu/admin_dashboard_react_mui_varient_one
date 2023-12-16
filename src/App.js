2d41544b8cdc569ff7934579d3286928424f5273
Madhumitha <madhumithavenu03@gmail.com>
2023-12-16 13:45:40 +0530

    adding path in app page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-// import Sidenav from './Sidenav';
+// import Sidenav from './components/Sidenav';
 import Home from './pages/Home';
 import About from './pages/About';
 import Settings from './pages/Settings';
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/about' exact element={<About />} />
-          <Route path='/settings' exact element={<Settings />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/about' exact element={<About />} />
+        <Route path='/settings' exact element={<Settings />} />
+      </Routes>
     </>
   );
 }
