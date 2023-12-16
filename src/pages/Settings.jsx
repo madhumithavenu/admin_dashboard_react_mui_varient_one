8199103e99634c6aa32a738714f8b9b7f8d8cf54
Madhumitha <madhumithavenu03@gmail.com>
2023-12-16 14:48:07 +0530

    Creating navbar

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,13 @@
 import { Box } from '@mui/material'
 import React from 'react'
-import Sidenav from '../Sidenav'
+import Sidenav from '../components/Sidenav'
+import Navbar from '../components/Navbar'
 
 export default function Settings() {
     return (
         <>
+            <Navbar />
+            <Box height={30} />
             <Box sx={{ display: "flex" }}>
                 <Sidenav />
                 <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
