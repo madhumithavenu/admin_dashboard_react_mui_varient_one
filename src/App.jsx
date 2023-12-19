7e63ddbe3eff954353fb3bc928ed8e28de60eead
Madhumitha <madhumithavenu03@gmail.com>
2023-12-19 12:51:45 +0530

    Adding products page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 // import Sidenav from './components/Sidenav';
 import Home from './pages/Home';
-import About from './pages/About';
 import Settings from './pages/Settings';
 import Analytics from './pages/Analytics';
+import Products from './pages/Products';
 // import Navbar from './components/Navbar';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' exact element={<Home />} />
-        <Route path='/about' exact element={<About />} />
+        <Route path='/products' exact element={<Products />} />
         <Route path='/settings' exact element={<Settings />} />
         <Route path='/analytics' exact element={<Analytics />} />
       </Routes>
